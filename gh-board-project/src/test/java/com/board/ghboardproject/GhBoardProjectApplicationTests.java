869c36253073b646e487a67bafbfc44306260a21
Geon-Hyeok <shon5982@gmail.com>
2023-10-16 22:49:02 +0900

    JPA 테스트 작성(임시)

---
+++ gh-board-project/src/test/java/com/board/ghboardproject/GhBoardProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.board.ghboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
