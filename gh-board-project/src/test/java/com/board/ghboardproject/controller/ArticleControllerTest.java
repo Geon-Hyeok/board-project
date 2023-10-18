8fc8ff4fab8cea11a918b7ff18182e1eaa0f1a24
Geon-Hyeok <shon5982@gmail.com>
2023-10-19 02:56:19 +0900

    #15 - 스프링 시큐리티 디펜던시 추가

--- gh-board-project/src/test/java/com/board/ghboardproject/controller/ArticleControllerTest.java
+++ gh-board-project/src/test/java/com/board/ghboardproject/controller/ArticleControllerTest.java
@@ -36,7 +36,6 @@ class ArticleControllerTest {
 
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
@@ -48,7 +47,7 @@ class ArticleControllerTest {
                 .andExpect((content().contentTypeCompatibleWith(MediaType.TEXT_HTML)))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
-                .andExpect(view().name("articleComments"));
+                .andExpect(model().attributeExists("articleComments"));
 
     }
 
